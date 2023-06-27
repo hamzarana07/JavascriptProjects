3cec735496bc4d67ee36b334219b83b9bfc1d5a1
Hamza Rana <hamzarana75@gmail.com>
2023-06-27 06:04:06 +0500

    Add 7-Questions

---
+++ 7- Questions/app.js
@@ -0,0 +1,42 @@
+//================================using selectors inside the element
+const questions = document.querySelectorAll('.question');
+questions.forEach(function(question){
+    const btn = question.querySelector('.question-btn')
+    // console.log(btn)
+    btn.addEventListener("click", function(){
+        questions.forEach(function(item){
+            if (item !== question) {
+                item.classList.remove("show-text")
+            }
+        });
+        question.classList.toggle("show-text")
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+// ==================================traversing the dom
+// const questionEl = document.querySelector(".question");
+// const questions = document.querySelectorAll(".question");
+// const questionTitle = document.querySelector(".question-title");
+// const questionBtn = document.querySelector(".question-btn");
+// const questionTxt = document.querySelector(".question-text");
+// const plusSpan = document.querySelector(".plus-icon");
+// const minusSpan = document.querySelector(".minus-icon");
+// const btns = document.querySelectorAll(".question-btn");
+
+// btns.forEach(function (btn) {
+//   btn.addEventListener("click", function (e) {
+//     let x = e.currentTarget.parentElement.parentElement;
+//     x.classList.toggle("show-text");
+//   });
+// });
